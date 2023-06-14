d9ab6b82b28dd01a7aacbbb921aabe81b95d4b1d
Kevin Do <do.kevin.vo@gmail.com>
2023-06-13 17:25:58 -0700

    :wrench: set base url based on environment dev or prod

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,10 +3,12 @@ import mdx from '@astrojs/mdx';
 
 import tailwind from '@astrojs/tailwind';
 
+const isDev = process.env.NODE_ENV === 'development';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://do-kevin.github.io',
-  base: '/pc-vn',
+  base: isDev ? '/' : '/pc-vn', // append /pc-vn for GitHub pages deployment
   integrations: [
     mdx({
       shikiConfig: {
